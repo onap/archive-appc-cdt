8f72e600e8b1d9b4038d9c2c32ab4269ff2f1df1
Skip Wonnell <skip@att.com>
2018-03-01 09:00:36 -0600

    Update files to conform to ONAP code style
    Fix indentation in two files, replacing tabs with spaces
    
    Issue-ID: APPC-687
    Change-Id: I6883ef86e8bfa19d627240dc0524a09dc6a16269
    Signed-off-by: Skip Wonnell <skip@att.com>

--- src/app/test/test.component.spec.ts
+++ src/app/test/test.component.spec.ts
@@ -45,114 +45,114 @@ import { NgProgress } from 'ngx-progressbar';
 import {NgProgressModule} from 'ngx-progressbar';
 
 describe( 'TestComponent', () => {
-	let component: TestComponent;
-	let fixture: ComponentFixture<TestComponent>;
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
 
 
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [TestComponent],
-			imports: [
-			FormsModule,
-			RouterTestingModule,
-			SimpleNotificationsModule,
-			HttpModule,
-			NgProgressModule
-			],
-			providers: [
-			NotificationService, 
-			ParamShareService,
-			MappingEditorService,
-			HttpUtilService,
-			UtilityService,
-			NgProgress
-			]
-		})
-		.compileComponents();
-	}));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestComponent],
+            imports: [
+            FormsModule,
+            RouterTestingModule,
+            SimpleNotificationsModule,
+            HttpModule,
+            NgProgressModule
+            ],
+            providers: [
+            NotificationService,
+            ParamShareService,
+            MappingEditorService,
+            HttpUtilService,
+            UtilityService,
+            NgProgress
+            ]
+        })
+        .compileComponents();
+    }));
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent(TestComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-	it( 'should create', () => {
-		expect(component).toBeTruthy();
-	});
+    it( 'should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-	// it('test', () => {
-	// 	expect(2).toEqual(1)
-	// });
-
-
-	// it('test preparfilename', inject([MappingEditorService], (mappingEditorService: MappingEditorService) => {
-	// 	// fixture = TestBed.createComponent(TestComponent);
-	// 	// component = fixture.componentInstance;
-	// 	mappingEditorService.latestAction=undefined
-		
-	// 		expect(component.prepareFileName()).toBe(undefined);
-	// 	}));
-
-	
-	it('Should have download method', () => {
-		expect(component.download).toBeDefined();
-	});
-
-	// Test abortTest Method
-	it('Should have abortTest method', () => {
-		expect(component.abortTest).toBeDefined();
-	});
-
-	it('Test abortTest Method', () => {
-		const temp = component.abortTest();
-		expect(component.enableBrowse).toBeTruthy();
-		expect(component.enableTestButton).toBeTruthy();
-	});
-
-	it('Should have excelBrowseOption method', () => {
-		expect(component.excelBrowseOption).toBeDefined();
-	});
-
-	it('Should have upload method', () => {
-		expect(component.upload).toBeDefined();
-	});
-
-	it('Should have constructTestPayload method', () => {
-		expect(component.constructTestPayload).toBeDefined();
-	});
-
-	it('Should have constructRequest method', () => {
-		expect(component.constructRequest).toBeDefined();
-	});
-
-	it('Should have testVnf method', () => {
-		expect(component.testVnf).toBeDefined();
-	});
-
-	it('Should have pollTestStatus method', () => {
-		expect(component.pollTestStatus).toBeDefined();
-	});
+    // it('test', () => {
+    //     expect(2).toEqual(1)
+    // });
 
 
-	// Test getUrlEndPoint Method
-	it('Should have getUrlEndPoint method', () => {
-		expect(component.getUrlEndPoint).toBeDefined();
-	});
+    // it('test preparfilename', inject([MappingEditorService], (mappingEditorService: MappingEditorService) => {
+    //     // fixture = TestBed.createComponent(TestComponent);
+    //     // component = fixture.componentInstance;
+    //     mappingEditorService.latestAction=undefined
 
-	it('getUrlEndPoint Should return value', () => {
-		expect(component.getUrlEndPoint('configmodify')).toEqual('config-modify');
-		expect(component.getUrlEndPoint('configbackup')).toEqual('config-backup');
-		expect(component.getUrlEndPoint('configrestore')).toEqual('config-restore');
-		expect(component.getUrlEndPoint('healthcheck')).toEqual('health-check');
-		expect(component.getUrlEndPoint('quiescetraffic')).toEqual('quiesce-traffic');
-		expect(component.getUrlEndPoint('resumetraffic')).toEqual('resume-traffic');
-		expect(component.getUrlEndPoint('startapplication')).toEqual('start-application');
-		expect(component.getUrlEndPoint('stopapplication')).toEqual('stop-application');
-		expect(component.getUrlEndPoint('upgradebackout')).toEqual('upgrade-backout');
-		expect(component.getUrlEndPoint('upgradepostcheck')).toEqual('upgrade-post-check');
-		expect(component.getUrlEndPoint('upgradeprecheck')).toEqual('upgrade-pre-check');
-		expect(component.getUrlEndPoint('upgradesoftware')).toEqual('upgrade-software');
-		expect(component.getUrlEndPoint('DeFaultCASE')).toEqual('defaultcase');
-	});
+    //         expect(component.prepareFileName()).toBe(undefined);
+    //     }));
+
+
+    it('Should have download method', () => {
+        expect(component.download).toBeDefined();
+    });
+
+    // Test abortTest Method
+    it('Should have abortTest method', () => {
+        expect(component.abortTest).toBeDefined();
+    });
+
+    it('Test abortTest Method', () => {
+        const temp = component.abortTest();
+        expect(component.enableBrowse).toBeTruthy();
+        expect(component.enableTestButton).toBeTruthy();
+    });
+
+    it('Should have excelBrowseOption method', () => {
+        expect(component.excelBrowseOption).toBeDefined();
+    });
+
+    it('Should have upload method', () => {
+        expect(component.upload).toBeDefined();
+    });
+
+    it('Should have constructTestPayload method', () => {
+        expect(component.constructTestPayload).toBeDefined();
+    });
+
+    it('Should have constructRequest method', () => {
+        expect(component.constructRequest).toBeDefined();
+    });
+
+    it('Should have testVnf method', () => {
+        expect(component.testVnf).toBeDefined();
+    });
+
+    it('Should have pollTestStatus method', () => {
+        expect(component.pollTestStatus).toBeDefined();
+    });
+
+
+    // Test getUrlEndPoint Method
+    it('Should have getUrlEndPoint method', () => {
+        expect(component.getUrlEndPoint).toBeDefined();
+    });
+
+    it('getUrlEndPoint Should return value', () => {
+        expect(component.getUrlEndPoint('configmodify')).toEqual('config-modify');
+        expect(component.getUrlEndPoint('configbackup')).toEqual('config-backup');
+        expect(component.getUrlEndPoint('configrestore')).toEqual('config-restore');
+        expect(component.getUrlEndPoint('healthcheck')).toEqual('health-check');
+        expect(component.getUrlEndPoint('quiescetraffic')).toEqual('quiesce-traffic');
+        expect(component.getUrlEndPoint('resumetraffic')).toEqual('resume-traffic');
+        expect(component.getUrlEndPoint('startapplication')).toEqual('start-application');
+        expect(component.getUrlEndPoint('stopapplication')).toEqual('stop-application');
+        expect(component.getUrlEndPoint('upgradebackout')).toEqual('upgrade-backout');
+        expect(component.getUrlEndPoint('upgradepostcheck')).toEqual('upgrade-post-check');
+        expect(component.getUrlEndPoint('upgradeprecheck')).toEqual('upgrade-pre-check');
+        expect(component.getUrlEndPoint('upgradesoftware')).toEqual('upgrade-software');
+        expect(component.getUrlEndPoint('DeFaultCASE')).toEqual('defaultcase');
+    });
 })
