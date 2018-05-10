6aeff854bd2f103740a33a1c0fc6f0d3e6b5d3b1
Kumar, Amaresh (ak583p) <ak583p@us.att.com>
2018-05-10 14:35:25 +0530

    initial commit for CDT proxy Service
    Initial commit for CDT proxy server to fix CORS Issues.
    
    Issue-ID: APPC-885
    Change-Id: Idf2688dbca3fd6c25636544147ed5b23a5a4ed7f
    Signed-off-by: Kumar, Amaresh (ak583p) <ak583p@us.att.com>

---
+++ CdtProxyService/src/main/java/com/cdt/service/SwaggerConfig.java
@@ -0,0 +1,60 @@
+/*
+============LICENSE_START==========================================
+===================================================================
+Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+===================================================================
+
+Unless otherwise specified, all software contained herein is licensed
+under the Apache License, Version 2.0 (the License);
+you may not use this software except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+============LICENSE_END============================================
+*/
+package com.cdt.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+/**
+ * Created by Amaresh Kumar on 09/May/2018.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.cdt.service.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("VNF Lifecycle Tetsing Api Doc")
+                .description("Developer API Reference guide for devlopong VNF lifecylye testing")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact("kamaresh@in.ibm.com")
+                .build();
+    }
+}
