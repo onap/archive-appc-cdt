58a091a1cac4bfd1b7ee367efed07d117a68e7a8
Sandeep J <sandeejh@in.ibm.com>
2018-07-13 15:10:51 +0530

    added setTimeOut method in PD retrival progres bar
    added setTimeOut of 3.5 seconds in progress bar for retrieving PD data from APPC
    
    Issue-ID: APPC-1073
    Change-Id: I79432be9b3f74dfa84e7a37da6fb8d86c2a779f5
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- src/app/vnfs/build-artifacts/parameter-definitions/parameter.component.ts
+++ src/app/vnfs/build-artifacts/parameter-definitions/parameter.component.ts
@@ -33,7 +33,6 @@ import { ParameterDefinitionService } from './parameter-definition.service';
 import 'rxjs/add/operator/map';
 import { NgProgress } from 'ngx-progressbar';
 
-
 let YAML = require('yamljs');
 
 declare var $: any;
@@ -254,7 +253,11 @@ export class ParameterComponent implements OnInit {
             if (this.paramShareService.getSessionParamData() != undefined && this.paramShareService.getSessionParamData().length > 0) {
                 this.getPDFromSession();
             } else {
+                this.ngProgress.start();
                 this.getPD();
+                setTimeout(() => {
+                this.ngProgress.done();
+            }, 3500);
             }
         } else {
             this.nService.error('Error', 'Please enter Action and VNF type in Reference Data screen');
@@ -275,7 +278,6 @@ export class ParameterComponent implements OnInit {
             }
         };
         let artifactContent: any;
-        this.ngProgress.start();
         return this.httpService.post({
             url: environment.getDesigns,
             data: input
@@ -286,14 +288,11 @@ export class ParameterComponent implements OnInit {
                 let fileModel = pdObject['vnf-parameter-list'];
                 this.displayParamObjects = this.parameterDefinitionService.populatePD(fileModel);
             }
-            else {
-
-            }
-            this.ngProgress.done();
+                       
         },
 
             error => this.nService.error('Error', 'Error in connecting APPC Server'));
-
+         
     }
 
     public getPDFromSession() {
@@ -340,7 +339,6 @@ export class ParameterComponent implements OnInit {
 
     //========================== End of ngOnDestroy() Method============================================
 
-    //========================== End of createOrUpdateParameterDefinitionData() Method============================================
     public showUpload() {
         this.selectedUploadType = this.uploadTypes[0].value;
     };
@@ -357,8 +355,7 @@ export class ParameterComponent implements OnInit {
                     this.displayParamObjects = this.parameterDefinitionService.processPDfile(this.myPdFileName, result);
                 }
             });
-        } else {
-            //this.notificationService.notifyErrorMessage('Failed to read file!Please try again.');
+            
         }
     }
 
