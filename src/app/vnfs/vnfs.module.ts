75ec41fec7f9a6439470ea4100266bc2040cdced
Sandeep J <sandeejh@in.ibm.com>
2018-07-13 13:22:42 +0530

    added progress spinner while uploading template
    added progress spinner while uploading template file so that the user can know visually that the file is being uploaded
    
    Issue-ID: APPC-1069
    Change-Id: I4d17a1f64a241dc3dc49154ec6c8abe7babb793a
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- src/app/vnfs/vnfs.module.ts
+++ src/app/vnfs/vnfs.module.ts
@@ -27,7 +27,7 @@ import {CommonModule} from '@angular/common';
 // modules
 import {VnfRoutingModule} from './vnf.routing';
 import {SharedModule} from '../shared/shared.module';
-
+import { NgxSpinnerModule } from 'ngx-spinner';
 import {AceEditorComponent} from 'ng2-ace-editor';
 // components
 import {MyvnfsComponent} from './myvnfs/myvnfs.component';
@@ -77,7 +77,7 @@ export const components = [
 
 export const modules = [
     CommonModule, VnfRoutingModule, SharedModule.forRoot(),
-    FormsModule, BootstrapModalModule, Ng2Bs3ModalModule, SimpleNotificationsModule.forRoot()
+    FormsModule, BootstrapModalModule, Ng2Bs3ModalModule, SimpleNotificationsModule.forRoot(),NgxSpinnerModule
 
 ];
 
