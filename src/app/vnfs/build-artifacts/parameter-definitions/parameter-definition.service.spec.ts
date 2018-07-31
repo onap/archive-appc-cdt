ac30d1bf8e6256ec4e5c04b665fe6ecd6c2187d3
Arundathi Patil <arundpil@in.ibm.com>
2018-07-31 14:31:40 +0530

    Test Case - Paramete-definitions.service
    Wrote test case to cover setValues method pf parameter-definitions
    service
    
    Issue-ID: APPC-1064
    Change-Id: I5e43aba3c6af733cf20922036ef7ad09b86a7cdd
    Signed-off-by: Arundathi Patil <arundpil@in.ibm.com>

--- src/app/vnfs/build-artifacts/parameter-definitions/parameter-definition.service.spec.ts
+++ src/app/vnfs/build-artifacts/parameter-definitions/parameter-definition.service.spec.ts
@@ -125,10 +125,17 @@ describe('ParameterDefinitionService', () => {
         service.downloadDataObject = {pd:{pdData:"", pdFileName:""}};
         service.destroy(displayParamObjects);
         expect(mappingEditorService.downloadDataObject.pd.pdData).toEqual(expectedPDdata);
-        //expect(mappingEditorService.appDataObject.pd).toEqual(expectedAppData);
-        
-
+        //expect(mappingEditorService.appDataObject.pd).toEqual(expectedAppData);        
+    
     }));
 
+    it('should test setValues method', inject([ParameterDefinitionService, ParamShareService, MappingEditorService], (service: ParameterDefinitionService, paramShareService: ParamShareService, mappingEditorService: MappingEditorService)=>{
+        service.setValues('vnfType-1', 'vnfcType-1', 'deviceProtocol', 'config', 'artifact-name');
+        expect(service.vnfType).toBe('vnfType-1');
+        expect(service.vnfcType).toBe('vnfcType-1');
+        expect(service.protocol).toBe('deviceProtocol');
+        expect(service.action).toBe('config');
+        expect(service.artifact_fileName).toBe('artifact-name');
+    }));
 
 });
